4d2e5b71aaf777c4f28f09c6448dec5c6300a6fa
mkt44plus-bit <mkt.44plus@gmail.com>
2026-09-18 21:39:12 +0700

    Add Android app and APK build workflow

---
+++ app/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+android {
+    namespace="com.datbeo.routermonitor"
+    compileSdk=35
+    defaultConfig {
+        applicationId="com.datbeo.routermonitor"
+        minSdk=26
+        targetSdk=35
+        versionCode=1
+        versionName="1.0.0"
+    }
+}
