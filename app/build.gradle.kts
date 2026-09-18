c3adddb4c339e91a1edb1802b769e36cd47f0f12
mkt44plus-bit <mkt.44plus@gmail.com>
2026-09-19 01:27:27 +0700

    Bump HĐB app version to 2.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId="com.datbeo.routermonitor"
         minSdk=26
         targetSdk=35
-        versionCode=4
-        versionName="2.0.0"
+        versionCode=5
+        versionName="2.1.0"
     }
 
     compileOptions {
