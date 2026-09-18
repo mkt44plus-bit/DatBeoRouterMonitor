6bd2089199564d7c00f6eb2834ea3fb042983a43
mkt44plus-bit <mkt.44plus@gmail.com>
2026-09-19 00:01:26 +0700

    Fix Web UI deployment script

--- openwrt/deploy_web_ui.sh
+++ openwrt/deploy_web_ui.sh
@@ -2,12 +2,16 @@
 set -eu
 
 BASE="https://raw.githubusercontent.com/mkt44plus-bit/DatBeoRouterMonitor/main/openwrt/www/datbeo"
+CGI="https://raw.githubusercontent.com/mkt44plus-bit/DatBeoRouterMonitor/main/openwrt/datbeo-traffic.cgi"
 mkdir -p /www/datbeo
 
 wget -qO /www/datbeo/index.html "$BASE/index.html"
 wget -qO /www/datbeo/style.css "$BASE/style.css"
 wget -qO /www/datbeo/app.js "$BASE/app.js"
+wget -qO /www/cgi-bin/datbeo-traffic "$CGI"
 
 chmod 0644 /www/datbeo/index.html /www/datbeo/style.css /www/datbeo/app.js
+chmod 0755 /www/cgi-bin/datbeo-traffic
 
-echo "DatBeo Web UI installed at http://$(ip -4 addr show wt0 2>/dev/null | awk '/inet /{sub(/\/.*,""$2);print $2;exit}')/datbeo/"
+IP="$(ip -4 addr show wt0 2>/dev/null | awk '/inet / {sub(/\/.*/, "", $2); print $2; exit}')"
+echo "DatBeo Web UI installed: http://${IP:-100.81.163.26}/datbeo/"
