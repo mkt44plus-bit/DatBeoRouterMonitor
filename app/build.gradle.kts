4275d80b8f8b247cbaa68b91d5d77922dfa69c71
mkt44plus-bit <mkt.44plus@gmail.com>
2026-09-18 21:48:21 +0700

    Fix Kotlin and Java JVM target compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,24 @@ plugins {
 android {
     namespace="com.datbeo.routermonitor"
     compileSdk=35
+
     defaultConfig {
         applicationId="com.datbeo.routermonitor"
         minSdk=26
         targetSdk=35
         versionCode=1
         versionName="1.0.0"
+    }
+
+    compileOptions {
+        sourceCompatibility=JavaVersion.VERSION_17
+        targetCompatibility=JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
     }
 }
